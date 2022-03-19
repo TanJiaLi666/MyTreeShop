2947b09069fac4302d89fbe744439fac9c3fd920
TanJiaLi <2331988047@qq.com>
2022-03-20 01:54:13 +0800

    订单设置
    Signed-off-by: TanJiaLi <2331988047@qq.com>

--- tuling-admin/src/main/java/com/tulingxueyuan/mall/modules/oms/controller/OmsOrderSettingController.java
+++ tuling-admin/src/main/java/com/tulingxueyuan/mall/modules/oms/controller/OmsOrderSettingController.java
@@ -1,6 +1,8 @@
 package com.tulingxueyuan.mall.modules.oms.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-01-25
  */
 @RestController
-@RequestMapping("/oms/omsOrderSetting")
+@RequestMapping("/orderSetting")
 public class OmsOrderSettingController {
+
+    /**
+     * export function getOrderSetting(id) {
+     *   return request({
+     *     url:'/orderSetting/'+id,
+     *     method:'get',
+     *   })
+     * }
+     *  id: null,
+     *     flashOrderOvertime: 0,
+     *     normalOrderOvertime: 0,
+     *     confirmOvertime: 0,
+     *     finishOvertime: 0,
+     *     commentOvertime: 0
+     */
+    @GetMapping("/{id}")
+    public void getOrderSetting(@PathVariable("id") Integer id) {
+
+    }
 
 }
 
